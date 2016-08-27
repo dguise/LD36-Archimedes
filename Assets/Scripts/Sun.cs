4ebb9c123da062d4a787d1251f566142a1e8f05c
Magnus Tullberg <magnus.tullberg@gmail.com>
2016-08-27 17:27:38 +0200

    fix better sun interaction

--- Assets/Scripts/Sun.cs
+++ Assets/Scripts/Sun.cs
@@ -7,6 +7,7 @@ namespace Assets.Scripts
 
         private GameObject[] mirrors;
         public float Speed = 1f;
+        public float Range = 10;
         // Use this for initialization
         void Start () {
             mirrors = GameObject.FindGameObjectsWithTag("Mirror");
@@ -20,6 +21,7 @@ namespace Assets.Scripts
                 RaycastBeam(gameObject.transform.position, mirror.transform.position);
             }
             transform.position = transform.position + Vector3.down * Time.deltaTime * Speed;
+            Debug.Log(1 << LayerMask.NameToLayer("Mirror"));
 
         }
 
@@ -27,23 +29,25 @@ namespace Assets.Scripts
         {
             RaycastHit mirrorHit;
             Vector3 direction = mirrorPos - sunPos;
-            Physics.Raycast(sunPos, direction, out mirrorHit, 1 << LayerMask.NameToLayer("Mirror"));
+            
+            Physics.Raycast(sunPos, direction, out mirrorHit, Mathf.Infinity, 1 << LayerMask.NameToLayer("Mirror"));
+            
 
             if (mirrorHit.collider)
             {
                 RaycastHit boatHit;
-                Physics.Raycast(mirrorHit.point, mirrorHit.normal * 100f, out boatHit, 1 << LayerMask.NameToLayer("Boat"));
+                Physics.Raycast(mirrorHit.point, mirrorHit.normal, out boatHit, Range, 1 << LayerMask.NameToLayer("Boat"));
 
                 if (boatHit.collider != null && boatHit.collider.tag == "Boat")
                 {
                     Debug.Log("boat 1 beam");
                 }
 
-                Debug.DrawRay(mirrorHit.point, mirrorHit.normal * 100f);
+                Debug.DrawRay(mirrorHit.point, mirrorHit.normal * Range);
             }
 
             Debug.DrawLine(sunPos, mirrorPos, Color.green);
-            Debug.Log(string.Format("SunPos: {0} - MirrorPos: {1} - Hit: {2} - ", sunPos, mirrorPos, mirrorHit.point));
+            //Debug.Log(string.Format("SunPos: {0} - MirrorPos: {1} - Hit: {2} - ", sunPos, mirrorPos, mirrorHit.point));
         }
 
 
