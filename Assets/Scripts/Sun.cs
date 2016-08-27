691a77adcb9084952a76aa734556416681773405
Magnus Tullberg <magnus.tullberg@gmail.com>
2016-08-27 18:53:21 +0200

    add sun movement affecting angle on beam

--- Assets/Scripts/Sun.cs
+++ Assets/Scripts/Sun.cs
@@ -18,32 +18,47 @@ namespace Assets.Scripts
         {
             foreach(GameObject mirror in mirrors)
             {
-                RaycastBeam(gameObject.transform.position, mirror.transform.position);
+                RaycastBeam(gameObject.transform.position, mirror.transform.position, mirror.transform.rotation);
+                
             }
             transform.position = transform.position + Vector3.down * Time.deltaTime * Speed;
-            Debug.Log(1 << LayerMask.NameToLayer("Mirror"));
 
         }
 
-        void RaycastBeam(Vector2 sunPos, Vector2 mirrorPos)
+        void RaycastBeam(Vector2 sunPos, Vector2 mirrorPos, Quaternion mirrorAngle)
         {
             RaycastHit mirrorHit;
             Vector3 direction = mirrorPos - sunPos;
             
             Physics.Raycast(sunPos, direction, out mirrorHit, Mathf.Infinity, 1 << LayerMask.NameToLayer("Mirror"));
+         
+
+            float normX = direction.normalized.x;
+            float normY = direction.normalized.y;
             
+            float angle = Mathf.Rad2Deg * Mathf.Acos(normX);
+            if (normY > 0)
+            {
+                angle *= -1;
+            }
+            Debug.Log(angle);
+            angle -= 180;
+
+            angle += (mirrorAngle.eulerAngles.z - 270) * 2;
+
+            Vector3 reflectionDirection = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle), Mathf.Sin(Mathf.Deg2Rad * angle), -1);
 
             if (mirrorHit.collider)
             {
-                RaycastHit boatHit;
-                Physics.Raycast(mirrorHit.point, mirrorHit.normal, out boatHit, Range, 1 << LayerMask.NameToLayer("Boat"));
+                 RaycastHit boatHit;
+                Physics.Raycast(mirrorHit.point, reflectionDirection, out boatHit, Range, 1 << LayerMask.NameToLayer("Boat"));
 
                 if (boatHit.collider != null && boatHit.collider.tag == "Boat")
                 {
                     Debug.Log("boat 1 beam");
                 }
 
-                Debug.DrawRay(mirrorHit.point, mirrorHit.normal * Range);
+                Debug.DrawRay(mirrorHit.point, reflectionDirection * Range);
             }
 
             Debug.DrawLine(sunPos, mirrorPos, Color.green);
