e28bbf70f39412a94a28b02d916debfec901c6cf
Maakep <schon.fredrik@gmail.com>
2016-08-27 14:08:34 +0200

    Initial commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class LevelManager : MonoBehaviour
+    {
+
+        private GameObject[] _spawnPoints;
+        private GameObject _sun;
+
+        // Use this for initialization
+        void Start ()
+        {
+            _spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+        }
+	
+        // Update is called once per frame
+        void Update () {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Sun.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Sun : MonoBehaviour
+    {
+
+        private GameObject mirror;
+
+        // Use this for initialization
+        void Start () {
+            mirror = GameObject.FindGameObjectWithTag("Mirror");
+        }
+	
+        // Update is called once per frame
+        void Update ()
+        {
+            Vector2 sunPos = gameObject.transform.position;        
+            Vector2 mirrorPos = mirror.transform.position;
+
+            RaycastHit mirrorHit;
+            Physics.Raycast(sunPos, mirrorPos, out mirrorHit, 1 << LayerMask.NameToLayer("Mirror"));
+
+            if (mirrorHit.collider)
+            {
+                RaycastHit boatHit;
+                Physics.Raycast(mirrorHit.point, mirrorHit.normal*100f, out boatHit, 1 << LayerMask.NameToLayer("Boat"));
+
+                if (boatHit.collider.tag == "Boat")
+                {
+                    Debug.Log("boat 1 beam");
+                }
+
+                Debug.DrawRay(mirrorHit.point, mirrorHit.normal*100f);
+            }
+
+            Debug.DrawLine(sunPos, mirrorHit.point, Color.green);
+            Debug.Log(string.Format("SunPos: {0} - MirrorPos: {1} - Hit: {2} - ", sunPos, mirrorPos, mirrorHit.point));
+        }
+
+    }
+}
+//var angle = Vector2.Angle(sunPos, mirrorPos);
